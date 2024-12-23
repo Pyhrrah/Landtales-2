4e736c3fd70f82df95070027c159b7c092cb911f
Bryan Mathurel <bmathurel3@myges.fr>
2024-12-23 12:38:27 +0100

    Correction du launcher et d'une variable de map.c

--- src/game/launcherMap.c
+++ src/game/launcherMap.c
@@ -4,10 +4,11 @@
 
 int main(int argc, char const *argv[]){
 
-    if (argc>2 && argv[2] == '1') system("rm -fr ./save[1-3]");
+    if (argc>2) if (strcmp(argv[2],"1")==0) system("rm -fr ./save[1-3]");
 
     char etage = 0;
     if (argc>1) sscanf(argv[1],"%hhd",&etage);
+    printf("Etage : %d\n", etage);
     
     if (etage < 1 || etage > 3) {
         for(char i=1; i<=3; i++){
@@ -16,6 +17,11 @@ int main(int argc, char const *argv[]){
             printf("Génération de l'étage %hhd : OK\n",i);
             printf("La seed est %d\n",lireSeed());
         }
+    } else {
+        supprimerEtage();
+        creerEtageEntier(etage);
+        printf("Génération de l'étage %hhd : OK\n",etage);
+        printf("La seed est %d\n",lireSeed());
     }
     
     printf("La génération de la map entière est un succès !\n");
